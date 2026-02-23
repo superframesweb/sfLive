ed65fc99b4e3fb3ec2a70e9f96e404076200d624
SuperFrames <superframesweb@gmail.com>
2026-02-23 08:54:56 +0530

    Delete src directory

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppComponent } from './app/app.component';
-import { appRouterProvider } from './app/app.config';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(), provideAnimations(), appRouterProvider]
-}).catch(err => console.error(err));
--- src/polyfills.ts
+++
@@ -1,2 +0,0 @@
-// Minimal polyfills
-import 'zone.js';
